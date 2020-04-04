ba59459e529a5b9ed98614802004d6db3b8d5921
AlekseyBykov <aleksey.n.bykov@gmail.com>
2020-04-04 17:26:10 +0400

    Refactoring.

--- src/test/java/alekseybykov/portfolio/xslt/XSLTTest.java
+++ src/test/java/alekseybykov/portfolio/xslt/XSLTTest.java
@@ -1,18 +1,11 @@
 package alekseybykov.portfolio.xslt;
 
-import org.apache.commons.io.IOUtils;
 import org.custommonkey.xmlunit.Diff;
 import org.junit.Test;
 import org.xml.sax.SAXException;
 
-import javax.xml.transform.Source;
-import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
-import javax.xml.transform.stream.StreamResult;
-import javax.xml.transform.stream.StreamSource;
-import java.io.File;
 import java.io.IOException;
-import java.io.StringWriter;
 
 import static org.junit.Assert.assertTrue;
 
@@ -20,18 +13,10 @@ public class XSLTTest extends XSLTTestBase {
 
 	@Test
 	public void testBaseTransformations() throws TransformerException, IOException, SAXException {
-		final String xslFileName = "base-transformations.xsl";
-		StringWriter writer = new StringWriter();
+		String transformed = transform("base-transformations.xsl");
+		String expected = loadFixture("transformed.xml");
 
-		Source xsltSource = new StreamSource(new File(classLoader.getResource(String.format("%s%s", "xsl/base/", xslFileName)).getFile()));
-		Transformer transformer = transformerFactory.newTransformer(xsltSource);
-		Source xmlSource = new StreamSource(new File(classLoader.getResource(String.format("%s%s", "xml/", xmlFileName)).getFile()));
-
-		transformer.transform(xmlSource, new StreamResult(writer));
-		String xmlTransformed = writer.toString();
-		String xmlExpected = IOUtils.toString(classLoader.getResourceAsStream("fixture/transformed.xml"));
-
-		Diff diff = new Diff(xmlExpected, xmlTransformed);
+		Diff diff = new Diff(expected, transformed);
 		assertTrue(diff.similar());
 	}
 }
--- src/test/java/alekseybykov/portfolio/xslt/XSLTTestBase.java
+++ src/test/java/alekseybykov/portfolio/xslt/XSLTTestBase.java
@@ -1,11 +1,20 @@
 package alekseybykov.portfolio.xslt;
 
+import org.apache.commons.io.IOUtils;
 import org.custommonkey.xmlunit.XMLUnit;
 import org.junit.BeforeClass;
 
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+import java.io.File;
+import java.io.IOException;
+import java.io.StringWriter;
 
-public class XSLTTestBase {
+public abstract class XSLTTestBase {
 
 	protected static TransformerFactory transformerFactory;
 	protected static ClassLoader classLoader;
@@ -17,6 +26,23 @@ public class XSLTTestBase {
 		XMLUnit.setNormalizeWhitespace(true);
 
 		transformerFactory = TransformerFactory.newInstance();
-		classLoader = XSLTTest.class.getClassLoader();
+		classLoader = XSLTTestBase.class.getClassLoader();
+	}
+
+	private Source loadXml() {
+		return new StreamSource(new File(classLoader.getResource(String.format("%s%s", "xml/", xmlFileName)).getFile()));
+	}
+
+	protected String loadFixture(String fixture) throws IOException {
+		return IOUtils.toString(classLoader.getResourceAsStream(String.format("%s%s", "fixture/", fixture)));
+	}
+
+	protected String transform(String xslt) throws TransformerException {
+		StringWriter writer = new StringWriter();
+		Source source = new StreamSource(new File(classLoader.getResource(String.format("%s%s", "xsl/base/", xslt)).getFile()));
+		Transformer transformer = transformerFactory.newTransformer(source);
+
+		transformer.transform(loadXml(), new StreamResult(writer));
+		return writer.toString();
 	}
 }
